ba9b4d145d08808d9c15b127a4275e9467ccc5af
yasin <yasinezziani@gmail.com>
2023-04-04 11:45:31 +0000

    practica II hecha y corregida

--- practica_laravel/src/tests/app/Infrastructure/Controller/GetUserControllerTest.php
+++ practica_laravel/src/tests/app/Infrastructure/Controller/GetUserControllerTest.php
@@ -2,23 +2,51 @@
 
 
 namespace Tests\app\Infrastructure\Controller;
+use App\Application\UserDataSource\UserDataSource;
+use App\Domain\User;
 use Tests\TestCase;
 
 class GetUserControllerTest extends TestCase
 {
+    private UserDataSource $userdata;
     /**
-     * @test
+     * @setUp
      */
-    public function userWithGivenEmailDoesNotExist(){
-        $response = $this->get('/api/user/email@email.com');
-        $response->assertNotFound();
-        $response->assertExactJson(['error'=>'usuario no encontrado']);
+    protected function setUp():void
+    {
+        parent::setUp();
+        $this->userdata = \Mockery::mock(UserDataSource::class);
+        $this->app->bind(UserDataSource::class, function () {
+            return $this->userdata;
+        });
     }
     /**
      * @test
      */
+    public function userWithGivenEmailDoesNotExist(){
+        $this->userdata
+            ->expects('findByEmail')
+            //->never();
+            ->with('email@email.com')
+            ->andReturnNull();
+        $response = $this->get('/api/user/email@email.com');
+        $response->assertNotFound();
+        $response->assertExactJson(['error'=>'usuario no encontrado']);
+    }
+   /* public function userWithGivenEmailDoesNotExist(){
+        $response = $this->get('/api/user/email@email.com');
+        $response->assertNotFound();
+        $response->assertExactJson(['error'=>'usuario no encontrado']);
+    }*/
+    /**
+     * @test
+     */
     public function userWithGivenEmailDoesExist(){
-        $response = $this->get('/api/user/email2@email.com');
+        $this->userdata
+            ->expects('findByEmail')
+            ->with('email@email.com')
+            ->andReturn(new User(1, "email@email.com"));
+        $response = $this->get('/api/user/email@email.com');
         $response->assertExactJson(['id' => '1', 'email' => 'email@email.com']);
     }
 }
